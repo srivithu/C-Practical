7fd1b0e5b37c6d76c1c2a455f553694cd22bac58
ucj-pizza-express <srivithursi@gmail.com>
2025-05-22 11:47:14 +0530

    initial setup

---
+++ SCM Practical/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SCM_Practical
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.lblDueDate = new System.Windows.Forms.Label();
+            this.lblTask = new System.Windows.Forms.Label();
+            this.txtDueDate = new System.Windows.Forms.TextBox();
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.ItemHeight = 25;
+            this.listBoxTasks.Location = new System.Drawing.Point(343, 81);
+            this.listBoxTasks.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(332, 129);
+            this.listBoxTasks.TabIndex = 9;
+            // 
+            // lblDueDate
+            // 
+            this.lblDueDate.AutoSize = true;
+            this.lblDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDueDate.Location = new System.Drawing.Point(18, 125);
+            this.lblDueDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDueDate.Name = "lblDueDate";
+            this.lblDueDate.Size = new System.Drawing.Size(119, 29);
+            this.lblDueDate.TabIndex = 7;
+            this.lblDueDate.Text = "Due Date";
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTask.Location = new System.Drawing.Point(18, 81);
+            this.lblTask.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(68, 29);
+            this.lblTask.TabIndex = 8;
+            this.lblTask.Text = "Task";
+            // 
+            // txtDueDate
+            // 
+            this.txtDueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDueDate.Location = new System.Drawing.Point(150, 122);
+            this.txtDueDate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDueDate.Name = "txtDueDate";
+            this.txtDueDate.Size = new System.Drawing.Size(184, 36);
+            this.txtDueDate.TabIndex = 5;
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTask.Location = new System.Drawing.Point(150, 77);
+            this.txtTask.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(184, 36);
+            this.txtTask.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(150, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 53);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 337);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.lblDueDate);
+            this.Controls.Add(this.lblTask);
+            this.Controls.Add(this.txtDueDate);
+            this.Controls.Add(this.txtTask);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.Label lblDueDate;
+        private System.Windows.Forms.Label lblTask;
+        private System.Windows.Forms.TextBox txtDueDate;
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SCM Practical/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCM_Practical
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string taskName = txtTask.Text;
+            string dueDate = txtDueDate.Text;
+
+            // No validation in v1.0
+            string taskEntry = taskName + " - Due: " + dueDate;
+            listBoxTasks.Items.Add(taskEntry);
+
+            // Clear inputs
+            txtTask.Clear();
+            txtDueDate.Clear();
+        }
+    }
+}
