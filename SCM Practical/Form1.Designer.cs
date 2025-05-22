c314ab689ebc80fe00dd3db374a9f2208098f29e
ucj-pizza-express <srivithursi@gmail.com>
2025-05-22 11:57:53 +0530

    Final Improvement

--- SCM Practical/Form1.Designer.cs
+++ SCM Practical/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.txtDueDate = new System.Windows.Forms.TextBox();
             this.txtTask = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBoxTasks
@@ -90,7 +92,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(150, 188);
+            this.button1.Location = new System.Drawing.Point(24, 232);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(113, 53);
@@ -99,11 +101,37 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(155, 232);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 53);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(281, 232);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 53);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(694, 337);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBoxTasks);
             this.Controls.Add(this.lblDueDate);
             this.Controls.Add(this.lblTask);
@@ -125,6 +153,8 @@
         private System.Windows.Forms.TextBox txtDueDate;
         private System.Windows.Forms.TextBox txtTask;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- SCM Practical/Form1.cs
+++ SCM Practical/Form1.cs
@@ -39,5 +39,24 @@ string.IsNullOrWhiteSpace(dueDateText))
             txtTask.Clear();
             txtDueDate.Clear();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtTask.Clear();
+            txtDueDate.Clear(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedIndex != -1)
+            {
+                listBoxTasks.Items.RemoveAt(listBoxTasks.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.", "Delete Task",
+        MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
     }
 }
